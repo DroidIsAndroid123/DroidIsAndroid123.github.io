e6bf2ec9dc47e7f5b87ddaaf984caf76f84d98f8
DroidIsAndroid123 <droid120501@gmail.com>
2023-09-28 22:27:20 +0500

    A counter

---
+++ count.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <limits>
+
+int main() {
+    int count = 1;
+    int lastValue = 0;
+
+    while (count < std::numeric_limits<int>::max()) {
+        lastValue = count;
+        count++;
+    }
+
+    std::cout << "Last Count: " << lastValue << std::endl;
+
+    return 0;
+}
